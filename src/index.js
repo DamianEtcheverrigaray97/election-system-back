046fc190a4fde8f1bba01341ae1ef6409bd21603
Damian Etcheverrigaray <damian.etcheverrigar@estudiantes.utec.edu.uy>
2025-01-23 15:59:18 -0300

    add CORS support to the application with specific configuration

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ routerConfig = require('./routes/index.routes');
 require('./config/db'); // Inicia la conexión a la base de datos
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./config/swagger');
+const cors = require('cors');
 
 const apiConfig = (app) => {
+    app.use(cors(corsOptions));
     app.use(bodyParser.json());
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
     return;
 } 
+
+const corsOptions = {
+    origin: 'http://localhost:4200',  // Permitir solicitudes solo desde este origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization', 'token'],  // Encabezados permitidos
+};
 
 const routesConfig = (app) => {
     app.use('/api/', routerConfig.routes_init());
@@ -18,7 +26,7 @@ const routesConfig = (app) => {
 
 const init = () => {
     const app = express();
-
+    
     apiConfig(app);
     routesConfig(app);
 
